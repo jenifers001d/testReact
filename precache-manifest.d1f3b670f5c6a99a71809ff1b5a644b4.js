dbabccc1fdf42c736fc637fc8a2691374c4d13a4
鄭竹容 <zhengzhurong@192-168-1-120.tpgi.com.au>
2019-07-18 14:51:01 +1000

    Updates

---
+++ precache-manifest.d1f3b670f5c6a99a71809ff1b5a644b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "730f491bb7a0208e5a440e6d1f7b6b63",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d9f94430610f1deba20",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "63b109b6e1b11087eeb5",
+    "url": "./static/js/2.e95e1186.chunk.js"
+  },
+  {
+    "revision": "9d9f94430610f1deba20",
+    "url": "./static/js/main.d00029b9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/testReact/precache-manifest.bd956b3fc78c5f3eede3306d925e9d97.js"
+  "./precache-manifest.d1f3b670f5c6a99a71809ff1b5a644b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/testReact/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
